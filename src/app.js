7b580e179a3000d4860790493d5cae969d3c5bba
Md.Foysal Hossain <fh375170@gmail.com>
2026-06-14 20:41:00 +0600

    so far done

--- src/app.js
+++ src/app.js
@@ -28,6 +28,32 @@ app.post("/notes", async (req, res) => {
 
 app.get("/notes", async (req, res) => {
 
+    const notes = await noteModel.find(); //alowes return array []
+
+    res.status(200).json({
+        message: "Notes fetched successfully",
+        data: notes
+    })
+})
+
+app.delete("/notes/:id", async (req, res) => {
+
+    const id = req.params.id;
+
+    await noteModel.findOneAndDelete({
+        _id: id
+    })
+})
+
+app.patch("/notes/:id", async (req, res) => {
+    const id = req.params.id;
+    const description = req.body.description;
+
+   await noteModel.findOneAndUpdate({ _id: id }, { description : description })
+
+   res.status(200).json({
+    message: "Note updated successfully"
+   })
 })
 
 module.exports = app;
