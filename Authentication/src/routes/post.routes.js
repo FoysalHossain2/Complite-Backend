3741391fb0d44d51d28d62841c64e52051d67dc0
Md.Foysal Hossain <fh375170@gmail.com>
2026-06-28 00:27:37 +0600

    done

---
+++ Authentication/src/routes/post.routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+
+const router = express.Router();
+
+
+router.post("/create", (req, res) => {
+
+    console.log(req.body);
+
+    console.log(req.cookies);
+    
+    res.send("Post created successfully")
+    
+})
+
+module.exports = router;
